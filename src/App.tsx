01d0438fd248fcce9f0271e625846cb757a9c82b
Marcos Souza <marcos.souza@beeleads.com.br>
2023-05-16 21:36:00 -0300

    feat: componente header

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { ThemeProvider } from "styled-components";
 
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { Transactions } from "./Transactions";
 
 function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>Hello world</h1>
+      <Transactions />
     </ThemeProvider>
   )
 }
